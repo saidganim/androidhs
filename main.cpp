83b784d1a2e8133baacceb4b072e30ded02323fa
PP course <cpu808694@gmail.com>
2018-11-21 05:54:21 +0100

    Stage2 almost done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include <GLES3/gl3.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 EGLDisplay display;
 EGLSurface pBuffer;
 EGLContext ctx;
 
- struct pagemap_entry{
+
+struct pagemap_entry{
     uint64_t pfn : 54;
     unsigned int soft_dirty : 1;
     unsigned int file_page : 1;
@@ -15,7 +21,21 @@ EGLContext ctx;
     unsigned int present : 1;
 };
 
-void* read_entry(int fd, void* va){
+
+struct kgsl_entry {
+	void* kgsl_gpu_va;	// the same as virtual address for embedded device
+	void* kgsl_va;		// no comments...
+	void* kgsl_pa;		// will be filled by us during one-way lookup
+	size_t kgsl_size;	// no comments...
+	uint64_t kgsl_id;	// id of GL object
+	char kgsl_flags[50];	// permissions mode
+	char kgsl_type[50];	// in our case everything is gpumem
+	char kgsl_usage[50];	// no comments...
+	size_t kgsl_sglen;	// number of pages taken by entry
+};
+
+
+uint64_t read_entry(int fd, void* va){
 	uint64_t data;
 	struct pagemap_entry res;
 	uint64_t pfn;
@@ -30,7 +50,7 @@ void* read_entry(int fd, void* va){
     // res.file_page = (data >> 61) & 1;
     // res.swapped = (data >> 62) & 1;
     // res.present = (data >> 63) & 1;
-	return (void*)( pfn * sysconf(_SC_PAGE_SIZE)) + ((uint64_t)va % sysconf(_SC_PAGE_SIZE));
+	return (uint64_t)( pfn * sysconf(_SC_PAGE_SIZE)) + ((uint64_t)va % sysconf(_SC_PAGE_SIZE));
 }
 
 const char *vertexShaderSource = "#version 300 es\n"
@@ -121,7 +141,10 @@ void egl_setup() {
 
 }
 
+
+
 int main(){
+	// #stage 1 vars
 	egl_setup();
 	unsigned int* textures_data = (unsigned int*) malloc(4096);
 	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -132,12 +155,33 @@ int main(){
 	GLint tex_uniform_location;
 	unsigned int readval[32][32];
 	int success;
-    char infoLog[512];
+    	char infoLog[512];
 	uint32_t rndX = rand() & 0b11111, rndY = rand() & 0b11111;
+	// #stage 2 vars
+	/*   Example of output of KGSL file
+	 *
+	 *  gpuaddr useraddr     size    id  flags       type            usage sglen
+ 	 *  b2e56000 b2e56000    65536    62 -r--pY     gpumem          command    16
+	 *  b440a000 b440a000     4096    55 ----pY     gpumem           any(0)     1
+	 *  b440c000 b440c000     4096    54 ----pY     gpumem           any(0)     1
+	 */
+
+	
+	int pagemap_f = open("/proc/self/pagemap", O_RDONLY);
+	char kgsl_path[100]; // no buffer overwlof here, please:)
+	struct kgsl_entry kgsl_arr[50];
+	struct kgsl_entry kgsl_cur;
+	pid_t self = getpid();
+	sprintf(kgsl_path, "/d/kgsl/proc/%d/mem", self);
+	FILE* kgsl_f = fopen(kgsl_path, "r");
+	size_t cur_i = 0;
+
+
+	// STAGE #1
 
 	memset(textures_data, 0x41, 4096);
 	memset(readval, 0xaa, 32 * 32 * sizeof(unsigned int));
-	GLuint VBO, tex, tex2;
+	GLuint VBO, tex, tex2[100];
 	// while(1){
 	
 	std::cout<<"Generated value sum==" << rndX + rndY << " ; rndX==" << rndX << "; rndY==" << rndY << "\n";
@@ -190,27 +234,28 @@ int main(){
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE){
 		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
 	}
-
+	uint32_t ttmp = 0;
 	// TEXTURE
-	glGenTextures(1, &tex2);
-	glBindTexture(GL_TEXTURE_2D, tex2);
-	uint32_t ttmp = rndX + rndY;
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 32, 32, 0, GL_RGBA, GL_UNSIGNED_BYTE, textures_data);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, rndX, rndY, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &ttmp);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+	for(int i = 0; i < 100; ++i){
+		glGenTextures(1, &tex2[i]);
+		glBindTexture(GL_TEXTURE_2D, tex2[i]);
+		ttmp = ttmp + rndX + rndY;
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 32, 32, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, rndX, rndY, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &ttmp);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	}
 	// DRAWING
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(shaderProgram);
 	tex_uniform_location = glGetUniformLocation(shaderProgram, "a_rnd");
 	glUniform2f(tex_uniform_location, rndX / 32.f,  rndY / 32.f);
 	tex_uniform_location = glGetUniformLocation(shaderProgram, "a_rnd2");
 	glUniform2f(tex_uniform_location, rndX,  rndY);
-	glBindTexture(GL_TEXTURE_2D, tex2);
+	glBindTexture(GL_TEXTURE_2D, tex2[0]);
 	glDrawArrays(GL_POINTS, 0, 1);
 
 	unsigned int* frame  = (unsigned int*)malloc(sizeof(unsigned int) * 32 * 32);
@@ -224,6 +269,34 @@ int main(){
 		}
 		printf("\n");
 	}
+
+	
+	// STAGE #2
+
+	if(!pagemap_f || !kgsl_f){
+		printf("Pagemap or KGSL interface is disabled( may be they both), please isntall all kernel modules or reinstall the kernel. See you later...");
+		exit(1);
+	}
+	fseek(kgsl_f, 73, SEEK_SET);
+	while(fscanf(kgsl_f, "%p %p     %lu    %lu  %s      %s            %s %lu\n", &kgsl_cur.kgsl_gpu_va,
+									 	&kgsl_cur.kgsl_va, &kgsl_cur.kgsl_size, &kgsl_cur.kgsl_id, kgsl_cur.kgsl_flags,
+										kgsl_cur.kgsl_type, kgsl_cur.kgsl_usage, &kgsl_cur.kgsl_sglen) != EOF){
+		if(strcmp(kgsl_cur.kgsl_usage, "texture"))
+			continue;
+		// we have texture
+		kgsl_cur.kgsl_pa = (void*)read_entry(pagemap_f, (void*)kgsl_cur.kgsl_va);
+		kgsl_arr[cur_i++] = kgsl_cur;
+		printf("%p %p     %lu    %3lu ", kgsl_cur.kgsl_gpu_va, kgsl_cur.kgsl_va, kgsl_cur.kgsl_size, kgsl_cur.kgsl_id);
+		printf("%s      ", kgsl_cur.kgsl_flags);
+		printf("%s            ", kgsl_cur.kgsl_type);
+		printf("%s ", kgsl_cur.kgsl_usage);
+		printf("%lu -- [physaddr:%p; virtualaddr:%p]\n", kgsl_cur.kgsl_sglen, kgsl_cur.kgsl_pa, kgsl_cur.kgsl_va);
+		fflush(stdout);
+	}
+	
+	
+
+
 	return 0;
 
 }
