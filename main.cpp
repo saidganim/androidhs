25c549be2098bd2d9957a318f0de39917eb5ba48
Saidgani Musaev <cpu808694@gmail.com>
2018-11-27 01:23:09 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <GLES3/gl3.h>
+#include <GLES2/gl2ext.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <unistd.h>
@@ -8,11 +9,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <math.h>
+// #include <EGL/gl2ext.h>
 
 EGLDisplay display;
 EGLSurface pBuffer;
 EGLContext ctx;
 
+typedef void (*glGetPerfMonitorGroupsAMD_t)(int*, size_t , uint*);
+typedef void (*glGetPerfMonitorCounterStringAMD_t)(uint, uint, size_t, size_t*, char*);
+typedef void (*glGetPerfMonitorGroupStringAMD_t)(uint, size_t, size_t*, char*);
+typedef void (*glGenPerfMonitorsAMD_t)(size_t, uint*);
+typedef void (*glGetPerfMonitorCountersAMD_t)(uint, int*, int*,size_t,uint*);
+typedef void (*glSelectPerfMonitorCountersAMD_t)(uint, bool, uint, int, uint*);
+typedef void (*glBeginPerfMonitorAMD_t)(uint);
+typedef void (*glEndPerfMonitorAMD_t)(uint monitor);
+typedef void (*glGetPerfMonitorCounterDataAMD_t)(uint, size_t, size_t, GLuint*, int*);
+typedef void (*glGetPerfMonitorCounterInfoAMD_t)(uint, uint, size_t, void*);
+
+ glGetPerfMonitorCountersAMD_t glGetPerfMonitorCountersAMD = (glGetPerfMonitorCountersAMD_t)eglGetProcAddress("glGetPerfMonitorCountersAMD");
+ glGetPerfMonitorGroupsAMD_t glGetPerfMonitorGroupsAMD = (glGetPerfMonitorGroupsAMD_t)eglGetProcAddress("glGetPerfMonitorGroupsAMD");
+ glGetPerfMonitorCounterStringAMD_t glGetPerfMonitorCounterStringAMD = (glGetPerfMonitorCounterStringAMD_t)eglGetProcAddress("glGetPerfMonitorCounterStringAMD");
+ glGetPerfMonitorGroupStringAMD_t glGetPerfMonitorGroupStringAMD = (glGetPerfMonitorGroupStringAMD_t)eglGetProcAddress("glGetPerfMonitorGroupStringAMD");
+ glGenPerfMonitorsAMD_t glGenPerfMonitorsAMD = (glGenPerfMonitorsAMD_t)eglGetProcAddress("glGenPerfMonitorsAMD");
+ glSelectPerfMonitorCountersAMD_t glSelectPerfMonitorCountersAMD = (glSelectPerfMonitorCountersAMD_t)eglGetProcAddress("glSelectPerfMonitorCountersAMD");
+ glBeginPerfMonitorAMD_t glBeginPerfMonitorAMD = (glBeginPerfMonitorAMD_t)eglGetProcAddress("glBeginPerfMonitorAMD");
+ glEndPerfMonitorAMD_t glEndPerfMonitorAMD = (glEndPerfMonitorAMD_t)eglGetProcAddress("glEndPerfMonitorAMD");
+ glGetPerfMonitorCounterDataAMD_t glGetPerfMonitorCounterDataAMD = (glGetPerfMonitorCounterDataAMD_t)eglGetProcAddress("glGetPerfMonitorCounterDataAMD");
+ glGetPerfMonitorCounterInfoAMD_t glGetPerfMonitorCounterInfoAMD = (glGetPerfMonitorCounterInfoAMD_t)eglGetProcAddress("glGetPerfMonitorCounterInfoAMD");
 
 struct pagemap_entry{
     uint64_t pfn : 54;
@@ -154,207 +177,123 @@ void egl_setup() {
 
 
 
+    typedef struct 
+    {
+            GLuint       *counterList;
+            int         numCounters;
+            int         maxActiveCounters;
+    } CounterInfo;
+
+    void
+    getGroupAndCounterList(GLuint **groupsList, int *numGroups, CounterInfo **counterInfo){
+        GLint          n;
+        GLuint        *groups;
+        CounterInfo   *counters;
+        glGetPerfMonitorGroupsAMD(&n, 0, NULL);
+        groups = (GLuint*) malloc(n * sizeof(GLuint));
+        glGetPerfMonitorGroupsAMD(NULL, n, groups);
+        *numGroups = n;
+        *groupsList = groups;
+        counters = (CounterInfo*) malloc(sizeof(CounterInfo) * n);
+        for (int i = 0 ; i < n; i++ ){
+            glGetPerfMonitorCountersAMD(groups[i], &counters[i].numCounters,&counters[i].maxActiveCounters, 0, NULL);
+            counters[i].counterList = (GLuint*)malloc(counters[i].numCounters * sizeof(int));
+            glGetPerfMonitorCountersAMD(groups[i], NULL, NULL, counters[i].numCounters, counters[i].counterList);
+        }
+		*counterInfo = counters;
+    }
+    
+    static int  countersInitialized = 0;
+        
+    int getCounterByName(char *groupName, char *counterName, GLuint *groupID, GLuint *counterID){
+        int          numGroups;
+        GLuint       *groups;
+        CounterInfo  *counters;
+        int          i = 0;
+        if (!countersInitialized){
+            getGroupAndCounterList(&groups, &numGroups, &counters);
+            countersInitialized = 1;
+        }
+        for ( i = 0; i < numGroups; i++ ){
+           char curGroupName[256];
+           glGetPerfMonitorGroupStringAMD(groups[i], 256, NULL, curGroupName);
+           if (strcmp(groupName, curGroupName) == 0){
+               *groupID = groups[i];
+               break;
+           }
+        }
+
+        if ( i == numGroups )
+            return -1;           // error - could not find the group name
+
+        for ( int j = 0; j < counters[i].numCounters; j++ ){
+            char curCounterName[256];
+            glGetPerfMonitorCounterStringAMD(groups[i], counters[i].counterList[j], 256, NULL, curCounterName);
+            if (strcmp(counterName, curCounterName) == 0){
+                *counterID = counters[i].counterList[j];
+                return 0;
+            }
+        }
+        return -1;           // error - could not find the counter name
+    }
+
+    void drawFrameWithCounters(void){
+        GLuint group[2];
+        GLuint counter[2];
+        GLuint monitor;
+        GLuint *counterData;
+
+        // Get group/counter IDs by name.  Note that normally the
+        // counter and group names need to be queried for because
+        // each implementation of this extension on different hardware
+        // could define different names and groups.  This is just provided
+        // to demonstrate the API.
+        getCounterByName("HW", "Hardware Busy", &group[0],&counter[0]);
+        getCounterByName("API", "Draw Calls", &group[1], &counter[1]);
+        // create perf monitor ID
+        glGenPerfMonitorsAMD(1, &monitor);
+        // enable the counters
+        glSelectPerfMonitorCountersAMD(monitor, GL_TRUE, group[0], 1,&counter[0]);
+        glSelectPerfMonitorCountersAMD(monitor, GL_TRUE, group[1], 1, &counter[1]);
+        glBeginPerfMonitorAMD(monitor);
+        // RENDER FRAME HERE
+        // ...
+        glEndPerfMonitorAMD(monitor);
+        // read the counters
+        GLint resultSize;
+        glGetPerfMonitorCounterDataAMD(monitor, GL_PERFMON_RESULT_SIZE_AMD, sizeof(GLint), (GLuint*)&resultSize, NULL);
+        counterData = (GLuint*) malloc(resultSize);
+        GLsizei bytesWritten;
+        glGetPerfMonitorCounterDataAMD(monitor, GL_PERFMON_RESULT_AMD,  resultSize, counterData, &bytesWritten);
+        // display or log counter info
+        GLsizei wordCount = 0;
+
+        while ( (4 * wordCount) < bytesWritten ){
+            GLuint groupId = counterData[wordCount];
+            GLuint counterId = counterData[wordCount + 1];
+            // Determine the counter type
+            GLuint counterType;
+            glGetPerfMonitorCounterInfoAMD(groupId, counterId, GL_COUNTER_TYPE_AMD, &counterType);
+            if ( counterType == GL_UNSIGNED_INT64_AMD ){
+                uint64_t counterResult = *(uint64_t*)(&counterData[wordCount + 2]);
+                // Print counter result
+                wordCount += 4;
+            }
+            else if ( counterType == GL_FLOAT ){
+                float counterResult = *(float*)(&counterData[wordCount + 2]);
+                // Print counter result
+                wordCount += 3;
+            }
+            // else if ( ... ) check for other counter types 
+            //   (GL_UNSIGNED_INT and GL_PERCENTAGE_AMD)
+        }
+    }
+
+
 int main(){
 	// #stage 1 vars
 	egl_setup();
-	unsigned int* textures_data = (unsigned int*) malloc(4096);
-	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	GLuint shaderProgram = glCreateProgram();
-	GLuint VAO;
-	GLuint FBF;
-	GLint tex_uniform_location;
-	unsigned int readval[32][32];
-	int success;
-    	char infoLog[512];
-	uint32_t rndX = rand() & 0b11111, rndY = rand() & 0b11111;
-	// #stage 2 vars
-	/*   Example of output of KGSL file
-	 *
-	 *  gpuaddr useraddr     size    id  flags       type            usage sglen
- 	 *  b2e56000 b2e56000    65536    62 -r--pY     gpumem          command    16
-	 *  b440a000 b440a000     4096    55 ----pY     gpumem           any(0)     1
-	 *  b440c000 b440c000     4096    54 ----pY     gpumem           any(0)     1
-	 */
-
-	char filebuffer[1024];
-	int pagemap_f = open("/proc/self/pagemap", O_RDONLY);
-	FILE* pagemap_f2 = fopen("/proc/self/pagemap", "r");
-	FILE*  progout = fopen("/data/local/tmp/mike/prog.out", "w");
-	char kgsl_path[100]; // no buffer overwlof here, please:)
-	struct kgsl_entry *kgsl_arr = NULL;
-	struct kgsl_entry kgsl_cur;
-	pid_t self = getpid();
-	sprintf(kgsl_path, "/d/kgsl/proc/%d/mem", self);
-	FILE* kgsl_f = fopen(kgsl_path, "r");
-	size_t cur_i = 0;
-	FILE* pagemap_csv = fopen("/data/local/tmp/mike/pagemap.dump", "w");
-	FILE* kgsl_csv = fopen("/data/local/tmp/mike/kgsl.csv", "w");
-	printf("SELF ID : %d\n", self);
-	// STAGE #1
-
-	memset(textures_data, 0x41, 4096);
-	memset(readval, 0xaa, 32 * 32 * sizeof(unsigned int));
-	GLuint VBO, tex, tex2;
-	// while(1){
-	
-	std::cout<<"Generated value sum==" << rndX + rndY << " ; rndX==" << rndX << "; rndY==" << rndY << "\n";
-
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
- 	if (success != GL_TRUE)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);   	
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (success != GL_TRUE)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		exit(1);
-    }
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
- 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		exit(1);
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	
-	// FRAMEBUFFER
-	glGenFramebuffers(1, &FBF);
-	glBindFramebuffer(GL_FRAMEBUFFER, FBF);
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 32, 32, 0, GL_RGBA, GL_UNSIGNED_BYTE, readval);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0); 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE){
-		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
-	}
-	uint32_t ttmp = 0;
-	// TEXTURE
-	glGenTextures(1, &tex2);
-	glBindTexture(GL_TEXTURE_2D, tex2);
-	ttmp = ttmp + rndX + rndY;
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 32, 32, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, rndX, rndY, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &ttmp);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	// DRAWING
-	
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    	glClear(GL_COLOR_BUFFER_BIT);
-	glUseProgram(shaderProgram);
-	tex_uniform_location = glGetUniformLocation(shaderProgram, "a_rnd");
-	glUniform2f(tex_uniform_location, rndX / 32.f,  rndY / 32.f);
-	tex_uniform_location = glGetUniformLocation(shaderProgram, "a_rnd2");
-	glUniform2f(tex_uniform_location, rndX,  rndY);
-	glBindTexture(GL_TEXTURE_2D, tex2);
-	glDrawArrays(GL_POINTS, 0, 1);
-
-	unsigned int* frame  = (unsigned int*)malloc(sizeof(unsigned int) * 32 * 32);
-	memset(frame, 0x00, 32 * 32 * sizeof(unsigned int));
-	glReadPixels(0, 0, 32, 32, GL_RGBA,GL_UNSIGNED_BYTE, frame);
-	printf("READVALS: \n");
-	for(int  i = 0; i < 32; ++i){
-		for( int j = 0; j < 32; ++j){
-			printf("%2d ", frame[i * 32 + j]);
-				
-		}
-		printf("\n");
-	}
-
-	
-	// STAGE #2
-
-	if(!pagemap_f || !kgsl_f){
-		printf("Pagemap or KGSL interface is disabled( may be they both), please isntall all kernel modules or reinstall the kernel. See you later...");
-		exit(1);
-	}
-	fseek(kgsl_f, 73, SEEK_SET);
-	fwrite("gpuaddr,useraddr,size,id,flags,type,usage,sglen\n", 1, 48, kgsl_csv);
-	while(fscanf(kgsl_f, "%p %p     %lu    %lu  %s      %s            %s %lu\n", &kgsl_cur.kgsl_gpu_va,
-									 	&kgsl_cur.kgsl_va, &kgsl_cur.kgsl_size, &kgsl_cur.kgsl_id, kgsl_cur.kgsl_flags,
-										kgsl_cur.kgsl_type, kgsl_cur.kgsl_usage, &kgsl_cur.kgsl_sglen) != EOF){
-		if(strcmp(kgsl_cur.kgsl_usage, "texture"))
-			continue;
-		// we have texture
-		kgsl_cur.kgsl_pa = (void*)read_entry(pagemap_f, (void*)kgsl_cur.kgsl_va);
-		
-		fprintf(kgsl_csv,"%p,%p,%lu,%u,", kgsl_cur.kgsl_gpu_va, kgsl_cur.kgsl_va, kgsl_cur.kgsl_size, kgsl_cur.kgsl_id);
-                fprintf(kgsl_csv,"%s,", kgsl_cur.kgsl_flags);
-                fprintf(kgsl_csv,"%s,", kgsl_cur.kgsl_type);
-                fprintf(kgsl_csv,"%s,", kgsl_cur.kgsl_usage);
-                fprintf(kgsl_csv,"%lu\n", kgsl_cur.kgsl_sglen);
-		
-		struct kgsl_entry** kgsl_ptr = &kgsl_arr;		
-		while(*kgsl_ptr){
-			if((*kgsl_ptr) && (*kgsl_ptr)->kgsl_pa > kgsl_cur.kgsl_pa)
-				break;
-			kgsl_ptr = &(*kgsl_ptr)->kgsl_next;
-		}
-		kgsl_cur.kgsl_next = (*kgsl_ptr);
-		*kgsl_ptr = (struct kgsl_entry*)malloc(sizeof(struct kgsl_entry));
-		**kgsl_ptr = kgsl_cur;
-	}
-
-	printf("\n=================================================SORTED LIST OF ENTRIES========================================\n\n");
-	unsigned int counter = 1;
-	unsigned int result = 0;
-	struct kgsl_entry* first = kgsl_arr;
-	kgsl_arr = kgsl_arr->kgsl_next;	
-	fprintf(progout, "group_id,id,useraddr,pfn,alloc_order\n");
-	while(kgsl_arr){
-		kgsl_cur = *kgsl_arr;
-		if((uint64_t)kgsl_arr->kgsl_pa - (uint64_t)first->kgsl_pa == counter * 0x1000){
-			++counter;
-		} else {
-			if( counter >= 64){
-				int target_level = 0;
-				unsigned int counter2 = counter;
-				while (counter2 >>= 1) ++target_level;
-				//fprintf(progout, "NEW GROUP OF TEXTURES: \n");
-				for(int i = 0; i < counter; ++i){
-					kgsl_cur = *first;
-					fprintf(progout, "mike,%d,%p,%p,%d\n", kgsl_cur.kgsl_id, kgsl_cur.kgsl_va, kgsl_cur.kgsl_pa, target_level); // only looks for order == 6
-					first = first->kgsl_next;
-				}
-				++result;
-			}
-			counter = 1;
-			first = kgsl_arr;
-		}
-	
-		kgsl_arr = kgsl_arr->kgsl_next;
-	}
-
-	fseek(pagemap_f2, 0, SEEK_SET);	
-	while(int readsize = fread(filebuffer, 1, 1024, pagemap_f2)){
-		fwrite(filebuffer, 1 , 1024, pagemap_csv);
-	};
-	
-	fclose(pagemap_csv);
-	fclose(kgsl_csv);
-	fclose(progout);
+	drawFrameWithCounters();
 	return 0;
 
 }
